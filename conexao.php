87ec6d2fb41a4ff5b6d9ea5afe2ab6b842e02a7e
Adriano Martins <48059813+AdrianoLM@users.noreply.github.com>
2021-11-26 16:02:49 -0300

    Add arquivo de conexão
    Adicionado arquivo conexao.PHP para realizar a communicação com o banco de dados na Amazon RDS

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname = "adrianogeraldo.c6pg5gf58ezz.us-east-1.rds.amazonaws.com";
+$bancodedados = "AdrianoGeraldo";
+$usuario = "root";
+$senha = "mackenzie";
+
+$conexao = new mysqli($hostname, $usuario, $senha, $bancodedados);
+if ($conexao ->connect_errno){
+    echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+else{
+    echo "Conexão efetuada com sucesso";
+}
